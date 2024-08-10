04d6d2be66bf5c27a353dfe34015e873db0959d8
Arthur <arthur>
2024-08-10 21:27:19 +0300

    added AEBF

--- src/main/java/net/mordgren/gtca/common/data/GTCAMachines.java
+++ src/main/java/net/mordgren/gtca/common/data/GTCAMachines.java
@@ -1,23 +1,42 @@
 package net.mordgren.gtca.common.data;
 
 import com.gregtechceu.gtceu.GTCEu;
+import com.gregtechceu.gtceu.api.GTCEuAPI;
+import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.data.RotationState;
+import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
 import com.gregtechceu.gtceu.api.machine.*;
+import com.gregtechceu.gtceu.api.machine.multiblock.CoilWorkableElectricMultiblockMachine;
+import com.gregtechceu.gtceu.api.machine.multiblock.PartAbility;
 import com.gregtechceu.gtceu.api.machine.multiblock.WorkableElectricMultiblockMachine;
 import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
+import com.gregtechceu.gtceu.api.pattern.MultiblockShapeInfo;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
 import com.gregtechceu.gtceu.api.recipe.OverclockingLogic;
-import com.gregtechceu.gtceu.common.data.GTBlocks;
-import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
+import com.gregtechceu.gtceu.common.data.*;
+import com.gregtechceu.gtceu.utils.FormattingUtil;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static com.gregtechceu.gtceu.api.GTValues.MV;
 import static com.gregtechceu.gtceu.api.pattern.Predicates.*;
-import static com.gregtechceu.gtceu.common.registry.GTRegistration.REGISTRATE;
+import static com.gregtechceu.gtceu.common.data.GTBlocks.CASING_INVAR_HEATPROOF;
+import static net.mordgren.gtca.GTCA.GTCA_REGISTRATE;
 
 public class GTCAMachines {
 
-    public static final MultiblockMachineDefinition steam_pressurizer = REGISTRATE.multiblock("steam_pressurizer", WorkableElectricMultiblockMachine::new)
+    public static final MultiblockMachineDefinition steam_pressurizer = GTCA_REGISTRATE.multiblock("steam_pressurizer", WorkableElectricMultiblockMachine::new)
             .rotationState(RotationState.NON_Y_AXIS)
             .recipeType(GTCARecipeTypes.STEAM_PRESSURIZER)
-            .appearanceBlock(GTBlocks.MACHINE_CASING_ULV)
+            .appearanceBlock(GTBlocks.CASING_STEEL_SOLID)
             .recipeModifier(GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.NON_PERFECT_OVERCLOCK))
             .pattern(definition ->
             FactoryBlockPattern.start()
@@ -37,5 +56,85 @@ public class GTCAMachines {
             )
             .register();
 
+    public static final MultiblockMachineDefinition ADVANCED_EBF = GTCA_REGISTRATE
+            .multiblock("advanced_ebf", CoilWorkableElectricMultiblockMachine::new)
+            .rotationState(RotationState.NON_Y_AXIS)
+            .recipeType(GTRecipeTypes.BLAST_RECIPES)
+            .recipeModifier(GTRecipeModifiers::ebfOverclock)
+            .recipeModifier(GTRecipeModifiers.PARALLEL_HATCH)
+            .recipeModifier(GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.PERFECT_OVERCLOCK))
+//            .recipeModifier(AEBFMod::aebfModifier)
+            .appearanceBlock(CASING_INVAR_HEATPROOF)
+            .pattern(definition -> FactoryBlockPattern.start()
+                    .aisle("XXXXX", "XCCCX", "XCCCX", "XCCCX", "#XXX#")
+                    .aisle("XXXXX", "C###C", "C###C", "C###C", "XXXXX")
+                    .aisle("XXXXX", "C###C", "C###C", "C###C", "XXHXX")
+                    .aisle("XXXXX", "C###C", "C###C", "C###C", "XXXXX")
+                    .aisle("XXSXX", "XCCCX", "XCCCX", "XCCCX", "#XMX#")
+                    .where('S', controller(blocks(definition.getBlock())))
+                    .where('X', blocks(CASING_INVAR_HEATPROOF.get()).setMinGlobalLimited(9)
+                            .or(autoAbilities(definition.getRecipeTypes()))
+                            .or(autoAbilities(false, false, true)))
+                    .where('H', abilities(PartAbility.MUFFLER))
+                    .where('M', abilities(PartAbility.MAINTENANCE))
+                    .where('C', heatingCoils())
+                    .where('#', air())
+                    .build()
+            )
+            .shapeInfos(definition -> {
+                List<MultiblockShapeInfo> shapeInfo = new ArrayList<>();
+                var builder = MultiblockShapeInfo.builder()
+                        .aisle("XXXEX", "XCCCX", "XCCCX", "XCCCX", "#XXX#")
+                        .aisle("IXXXF", "C###C", "C###C", "C###C", "XXXXX")
+                        .aisle("OXXXD", "C###C", "C###C", "C###C", "XXHXX")
+                        .aisle("XXXXX", "C###C", "C###C", "C###C", "XXXXX")
+                        .aisle("XXSXX", "XCCCX", "XCCCX", "XCCCX", "#XMX#")
+                        .where('X', CASING_INVAR_HEATPROOF.getDefaultState())
+                        .where('S', definition, Direction.SOUTH)
+                        .where('#', Blocks.AIR.defaultBlockState())
+                        .where('E', GTMachines.ENERGY_INPUT_HATCH[GTValues.LV], Direction.NORTH)
+                        .where('I', GTMachines.ITEM_IMPORT_BUS[GTValues.LV], Direction.WEST)
+                        .where('O', GTMachines.ITEM_EXPORT_BUS[GTValues.LV], Direction.WEST)
+                        .where('F', GTMachines.FLUID_IMPORT_HATCH[GTValues.LV], Direction.EAST)
+                        .where('D', GTMachines.FLUID_EXPORT_HATCH[GTValues.LV], Direction.EAST)
+                        .where('H', GTMachines.MUFFLER_HATCH[GTValues.LV], Direction.UP)
+                        .where('M', GTMachines.MAINTENANCE_HATCH, Direction.SOUTH);
+                GTCEuAPI.HEATING_COILS.entrySet().stream()
+                        .sorted(Comparator.comparingInt(entry -> entry.getKey().getTier()))
+                        .forEach(
+                                coil -> shapeInfo.add(builder.shallowCopy().where('C', coil.getValue().get()).build()));
+                return shapeInfo;
+            })
+            .recoveryItems(
+                    () -> new ItemLike[] { GTItems.MATERIAL_ITEMS.get(TagPrefix.dustTiny, GTMaterials.Ash).get() })
+            .workableCasingRenderer(
+                    GTCEu.id("block/casings/solid/machine_casing_heatproof"),
+                    GTCEu.id("block/multiblock/electric_blast_furnace"),
+                    true
+            )
+            .tooltips(
+                    Component.translatable("gtceu.multiblock.parallelizable.tooltip"),
+                    Component.translatable("gtceu.machine.available_recipe_map_1.tooltip", "Electric Blast Furnace")
+            )
+            .additionalDisplay((controller, components) -> {
+                if (controller instanceof CoilWorkableElectricMultiblockMachine coilMachine && controller.isFormed()) {
+                    components.add(Component.translatable("gtceu.multiblock.blast_furnace.max_temperature",
+                            Component
+                                    .translatable(
+                                            FormattingUtil
+                                                    .formatNumbers(coilMachine.getCoilType().getCoilTemperature() +
+                                                            100L * Math.max(0, coilMachine.getTier() - GTValues.MV)) +
+                                                    "K")
+                                    .setStyle(Style.EMPTY.withColor(ChatFormatting.RED))));
+                }
+            })
+            .compassSections(GTCompassSections.TIER[MV])
+            .compassNodeSelf()
+            .register();
+
+
     public static void init() {}
+
 }
+
+
