b6e0657e4a331e9c33256170e6800622930da875
Arthur <arthur>
2024-08-25 10:12:10 +0300

    Chem gen recipe constructor

--- src/main/java/net/mordgren/gtca/common/data/fuel/recipes/ChemGenFuels.java
+++ src/main/java/net/mordgren/gtca/common/data/fuel/recipes/ChemGenFuels.java
@@ -1,14 +1,22 @@
 package net.mordgren.gtca.common.data.fuel.recipes;
 
+import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
+import com.lowdragmc.lowdraglib.side.fluid.FluidStack;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.mordgren.gtca.common.data.GTCAMaterials;
+import net.mordgren.gtca.common.data.GTCARecipeTypes;
+
+import java.util.ArrayList;
 import java.util.function.Consumer;
+
+import static java.lang.Integer.parseInt;
 
 public class ChemGenFuels {
     public static void init(Consumer<FinishedRecipe> provider) {
         DiisononylPhthalate(provider);
+        chemGenRecipes(provider);
     }
 
     private static void DiisononylPhthalate (Consumer<FinishedRecipe> provider) {
@@ -19,5 +27,29 @@ public class ChemGenFuels {
                 .outputFluids(GTCAMaterials.DiisononylPhthalate.getFluid(3000))
                 .outputFluids(GTMaterials.Hydrogen.getFluid(1000))
                 .save(provider);
+    }
+
+    private static ArrayList<Object[]> Fuels;
+
+    private static void fuelsArrayInit() {
+        Fuels = new ArrayList<>();
+
+        /// recipe name (костыль), fuel, amount, duration
+        Fuels.add(new Object[]{"dsa", GTMaterials.DilutedSulfuricAcid, 20, 20});
+    }
+
+    private static void chemGenRecipes(Consumer<FinishedRecipe> provider) {
+        fuelsArrayInit();
+        for(Object[] fuelType : Fuels){
+            int duration = parseInt(fuelType[3].toString());
+            long amount = parseInt(fuelType[2].toString());
+            FluidStack Fuel = ((Material)fuelType[1]).getFluid(amount);
+            GTCARecipeTypes.CHEMICAL_GENERATOR.recipeBuilder(fuelType[0].toString())
+                    .EUt(-32)
+                    .duration(duration)
+                    .inputFluids(Fuel)
+                    .save(provider);
+
+        }
     }
 }
