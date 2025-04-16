8107b40d6d07ca0f5210715f833ad969bb4fa01e
mordgren <art01918@gmail.com>
2025-04-17 02:03:49 +0300

    fixed build crash

--- src/main/java/net/mordgren/gtca/common/data/recipes/BastnasiteLine.java
+++ src/main/java/net/mordgren/gtca/common/data/recipes/BastnasiteLine.java
@@ -11,13 +11,13 @@ import java.util.function.Consumer;
 
 public class BastnasiteLine {
     public static void init(Consumer<FinishedRecipe> provider){
-        GTCARecipeTypes.THERMAL_REACTOR.recipeBuilder("crushed_ore_to_muddy").EUt(1920).duration(400)
-                .blastFurnaceTemp(800)
-                .inputItems(GTCAHelper.getItem("crushedOre", GTMaterials.Bastnasite, 2))
-                .inputFluids(GTMaterials.NitricAcid.getFluid(700))
-                .outputItems(GTMaterials.SiliconDioxide, 1)
-                .outputFluids(GTCAMaterials.MuddyBastnasiteRareEarthSolution.getFluid(400))
-                .save(provider);
+//        GTCARecipeTypes.THERMAL_REACTOR.recipeBuilder("crushed_ore_to_muddy").EUt(1920).duration(400)
+//                .blastFurnaceTemp(800)
+//                .inputItems(GTCAHelper.getItem("crushedOre", GTMaterials.Bastnasite, 2))
+//                .inputFluids(GTMaterials.NitricAcid.getFluid(700))
+//                .outputItems(GTMaterials.SiliconDioxide, 1)
+//                .outputFluids(GTCAMaterials.MuddyBastnasiteRareEarthSolution.getFluid(400))
+//                .save(provider);
 
     }
 }
